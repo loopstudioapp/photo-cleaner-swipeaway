706d3166ce53c81785e98fcf489b82707c4f6917
voltec <voltec366@gmail.com>
2026-01-18 21:15:02 +0400

    feat: configure Singular SKAdNetwork support
    - Enable SKAdNetwork for iOS attribution
    - Add NSUserTrackingUsageDescription for future ATT support
    - Remove waitForTrackingAuthorizationWithTimeoutInterval to send events immediately

--- services/SingularService.ts
+++ services/SingularService.ts
@@ -61,9 +61,6 @@ class SingularService {
       // Enable SKAdNetwork (required for iOS attribution)
       config.skAdNetworkEnabled = true;
 
-      // Wait for ATT authorization before sending first session (iOS 14.5+)
-      config.waitForTrackingAuthorizationWithTimeoutInterval = 300; // 300 seconds
-
       if (__DEV__) {
         config.withLoggingEnabled();
       }
